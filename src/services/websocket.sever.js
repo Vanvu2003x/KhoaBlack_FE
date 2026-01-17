9b405953b857fecd810d633445060a7cda3fcbf1
Vanvu <2003vanvu2006@gmail.com>
2026-01-18 00:57:49 +0700

    done 2

--- src/services/websocket.sever.js
+++ src/services/websocket.sever.js
@@ -4,7 +4,7 @@ let socket = null;
 
 export const connectSocket = (token, onBalanceUpdate) => {
   // Token is ignored as we use cookies now
-  const URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+  const URL = process.env.NEXT_PUBLIC_API_URL;
 
   if (!socket) {
     socket = io(URL, {
